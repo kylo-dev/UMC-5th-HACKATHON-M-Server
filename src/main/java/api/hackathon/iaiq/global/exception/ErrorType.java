8d3082d6ea2d09655c185011fe1b809b974907f3
toychip <dlawnsgud427@naver.com>
2024-01-02 19:36:24 +0900

    feat: QuestionRequest (#7)

--- src/main/java/api/hackathon/iaiq/global/exception/ErrorType.java
+++ src/main/java/api/hackathon/iaiq/global/exception/ErrorType.java
@@ -37,6 +37,10 @@ public enum ErrorType {
     _USER_NOT_FOUND_DB(NOT_FOUND, "USER_4041", "존재하지 않는 회원입니다."),
 
 
+    // ------------------------------------------ CATEGORY ------------------------------------------
+
+    _CATEGORY_INVALID_REQUEST(BAD_REQUEST, "CATEGORY_4000", "잘못된 카테고리 입니다."),
+
     ;
 
     private final HttpStatus status;
