3d1a54940e0e863a959b7d00c178e614df1fc893
toychip <dlawnsgud427@naver.com>
2024-01-03 03:19:24 +0900

    docs: Swagger request field 수정 (#11)

--- src/main/java/api/hackathon/iaiq/domain/question/answer/dto/request/SearchCondition.java
+++ src/main/java/api/hackathon/iaiq/domain/question/answer/dto/request/SearchCondition.java
@@ -6,7 +6,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import lombok.Getter;
 
+@Getter
 public class SearchCondition {
 
     @Schema(description = "답 년도", example = "2023")
@@ -18,6 +20,7 @@ public class SearchCondition {
     @Schema(description = "답 일", example = "25")
     public String day;
 
+    @Schema(hidden = true)
     public LocalDate getSearchDate() {
         try {
             String dateString = String.format("%s-%s-%s", year, month, day);
