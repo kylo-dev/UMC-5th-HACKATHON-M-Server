a0e53944e32b5d8450723c7d8a93e1156411d421
toychip <dlawnsgud427@naver.com>
2024-01-03 01:50:33 +0900

    feat: Answer Member 연관관계 매핑 (#11)

--- src/main/java/api/hackathon/iaiq/domain/question/answer/domain/Answer.java
+++ src/main/java/api/hackathon/iaiq/domain/question/answer/domain/Answer.java
@@ -1,13 +1,16 @@
 package api.hackathon.iaiq.domain.question.answer.domain;
 
 
+import api.hackathon.iaiq.domain.Member.domain.Member;
 import api.hackathon.iaiq.domain.question.domain.Question;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -31,6 +34,10 @@ public class Answer {
     @OneToOne
     @JoinColumn(name = "question_id")
     private Question question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     private String formatDate;
 }
