37c35ebbf5b1b9df0c9334f85faa9f91c889117e
tnals2384 <tnals2384@gmail.com>
2024-01-02 18:50:35 +0900

    feat : Member Entity, Repository 생성 (#3)

---
+++ src/main/java/api/hackathon/iaiq/domain/Member/domain/Member.java
@@ -0,0 +1,25 @@
+package api.hackathon.iaiq.domain.Member.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String profileImgUrl;
+}
---
+++ src/main/java/api/hackathon/iaiq/domain/Member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package api.hackathon.iaiq.domain.Member.repository;
+
+import api.hackathon.iaiq.domain.Member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+    Optional<Member> findMemberByEmailAndProvider(String email, String Provider);
+}
