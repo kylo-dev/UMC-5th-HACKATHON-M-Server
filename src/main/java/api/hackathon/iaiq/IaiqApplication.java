ef1da17962991ad066ea6e6fddbfdc20887ff7a3
toychip <dlawnsgud427@naver.com>
2024-01-02 15:05:29 +0900

    Initial commit

---
+++ src/main/java/api/hackathon/iaiq/IaiqApplication.java
@@ -0,0 +1,13 @@
+package api.hackathon.iaiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaiqApplication.class, args);
+	}
+
+}
